7025b23859f696c30ab190c43bb131b752c7e819
Matt Jensen <mattj65816@gmail.com>
2023-10-21 15:10:22 -0500

    feat: FlightDisplay.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import type {AircraftPosition, FlightPosition, GroundPosition} from "./flight-types";
 import {isFlightPosition, isGroundPosition} from "./flight-types";
 import {AircraftPositionProvider, useAircraftPosition} from "./AircraftPositionProvider";
+import {FlightDisplay} from "./FlightDisplay";
 
 /* Package exports. */
 export {
     AircraftPosition,
     AircraftPositionProvider,
+    FlightDisplay,
     FlightPosition,
     GroundPosition,
     isFlightPosition,
